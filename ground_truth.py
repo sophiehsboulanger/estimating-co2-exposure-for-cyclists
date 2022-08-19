32a0630b3fa2d23a23edde248df104bad19f831c
sophie-jesslaptop <sophiehsboulanger@outlook.com>
2022-08-19 19:23:47 +0100

    corrected accuracy calculation and saves output df

--- ground_truth.py
+++ ground_truth.py
@@ -3,14 +3,17 @@ import counter
 
 # gt = ground truth
 gt_csv = "ground_truth/ground_truth.csv"
-gt_df = pd.read_csv(gt_csv)
+gt_df = pd.read_csv(gt_csv, na_values=0)
 
 for i in range(len(gt_df)):
     video_in = "ground_truth/gt_in/" + gt_df.loc[i, "file name"]
     video_out = "ground_truth/gt_out/" + gt_df.loc[i, "file name"]
     gt_df.loc[i, 'counted'] = counter.main(video_in, video_out)
-    error_rate = (abs(gt_df.loc[i, 'counted'] - gt_df.loc[i, 'count'])) / ((gt_df.loc[i, 'count']) * 100)
-    gt_df.loc[i, 'accuracy'] = 100 - error_rate
+    accuracy = abs(gt_df.loc[i, 'counted'] - gt_df.loc[i, 'count'])
+    accuracy = accuracy/gt_df.loc[i, 'count']
+    accuracy = accuracy * 100
+    accuracy = 100 - accuracy
+    gt_df.loc[i, 'accuracy'] = accuracy
 
 print(gt_df)
-
+gt_df.to_csv(path_or_buf="ground_truth/gt_out/gt_out.csv", index=False)
