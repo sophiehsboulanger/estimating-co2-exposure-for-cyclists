f5956c43ff4699c49377658e9deee7773f0a1c2a
sophie desktop <sophiehsboulanger@outlook.com>
2023-02-27 12:19:00 +0000

    created vehicle_distance.py to calculate the distance from the camera to the vehicle

--- vehicle_distance.py
+++ vehicle_distance.py
@@ -1,97 +1,106 @@
-# adapted from https://www.section.io/engineering-education/license-plate-detection-and-recognition-using-opencv-and
-# -pytesseract/
+# adapted from https://www.section.io/engineering-education/license-plate-detection-and-recognition-using-opencv-and-pytesseract/
+# https://pyimagesearch.com/2015/01/19/find-distance-camera-objectmarker-using-python-opencv/
 import cv2
+import numpy as np
+import torch
 
 
 class VehicleDistanceCalculator:
-    def __init__(self, lp_min_ar=4, lp_max_ar=5, lt_min_ar=0.5, lt_max_ar=0.7, debug=False, accuracy=2):
-        self.lp_min_ar = lp_min_ar
-        self.lp_max_ar = lp_max_ar
-        self.lt_min_ar = lt_min_ar
-        self.lt_max_ar = lt_max_ar
+    def __init__(self, min_ar=0.55, max_ar=0.75, debug=False, accuracy=2):
+
+        self.min_ar = min_ar
+        self.max_ar = max_ar
         self.debug = debug
         self.accuracy = accuracy
+        self.focal_length = 0
+        self.known_height = 79
 
     def debug_imshow(self, img, title):
         cv2.imshow(title, img)
         if self.debug:
             cv2.waitKey(0)
 
-    def find_contours(self, img):
-        # convert to grey
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        self.debug_imshow(img, "grey")
+    def find_licence_plate(self, img):
+        lp_detector = torch.hub.load('C:/Users/sophi/PycharmProjects/licencePlateDetector/yolov5', 'custom',
+                                     path='C:/Users/sophi/PycharmProjects/licencePlateDetector/yolov5/runs/train/exp7/weights/best.pt',
+                                     source='local')  # default
+        lp_detector.conf = 0.6
 
+        # detect the licence plate, should only be one
+        results = lp_detector(img)
+        # results.show()
+        results = results.xyxy[0].cpu().numpy()
+        # print(results)
+        for result in results:
+            # get xmin, ymin, xmax, ymax of the bb for the licence plate
+            # xyxy = results.xyxy[0].cpu().numpy()
+            # xyxy = xyxy[0]
+            xmin, ymin, xmax, ymax, *rest = np.rint(result).astype('int')
+            ar = round((xmax - xmin) / float((ymax - ymin)), 2)
+            if 4 <= ar <= 6:
+                # return the crop of the licence plate
+                return img[ymin:ymax, xmin:xmax]
+
+    def get_average_character_height(self, lp):
+        heights = []
+        # convert to grey
+        img = cv2.cvtColor(lp, cv2.COLOR_BGR2GRAY)
         # denoise
         img = cv2.fastNlMeansDenoising(img, h=10)
-        self.debug_imshow(img, "denoise")
-
         # detect edges
         edged = cv2.Canny(img, 20, 200)
-        self.debug_imshow(edged, 'edges')
-
         # find contours
         contours, new = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-        image1 = img.copy()
-        cv2.drawContours(image1, contours, -1, (0, 255, 0), 3)
-        self.debug_imshow(image1, 'all contours')
-
-        # take the top 30 contours, based on area, and sort them (largest to smallest)
-        contours = sorted(contours, key=cv2.contourArea, reverse=True)#[:30]
-        image2 = img.copy()
-        cv2.drawContours(image2, contours, -1, (0, 255, 0), 3)
-        self.debug_imshow(image2, 'top 30 contours')
-        return contours
-
-    def get_character_height(self, img):
-        found = False
-        # iterate through the contours
-        for c in self.find_contours(img):
-            # approximate bounding contour
-            # perimeter = cv2.arcLength(c, True)
-            # approx = cv2.approxPolyDP(c, 0.018 * perimeter, True)
-            # # if the contour has four sides
-            # if len(approx) == 4:
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)[:20]
+        for contour in contours:
             # get the bounding box
-            x, y, w, h = cv2.boundingRect(c)
+            x, y, w, h = cv2.boundingRect(contour)
             # calculate the aspect ratio
-            ar = round(w / float(h))
+            ar = round(w / float(h), 2)
+            # print(ar)
+            # img2 = img.copy()
+            # cv2.rectangle(img2, (x, y), (x + w, y + h), (0, 255, 0), 1)
+            # cv2.putText(img2, str(ar), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color=(0, 255, 0), thickness=1, )
+            # cv2.imshow("fdsf", img2)
+            # cv2.waitKey(0)
             # if it's between the accepted aspect ratios
-            if self.lp_min_ar <= ar <= self.lp_max_ar:
-                found = True
-                lp = img[y:y + h, x:x + w]
-                self.debug_imshow(lp, 'licence plate')
-                # exit the loop as we've found the licence plate
-                break
-        if not found:
-            print("no licence plate found")
-            return found
-        elif found:
-            found = False
-            # variable to hold bounding boxes
-            heights = []
-            # now we've found the licence plate, isolate the characters within
-            for c2 in self.find_contours(lp):
-                # get the bounding box
-                x, y, w, h = cv2.boundingRect(c2)
-                # calculate the aspect ratio
-                ar = round(w / float(h), 2)
-                if self.lt_min_ar <= ar <= self.lt_max_ar:  # if it's between the accepted aspect ratios
-                    found = True
-                    cv2.rectangle(lp, (x, y), (x + w, y + h), color=(0, 255, 0), thickness=3)
-                    heights.append(h)
-            if found:
-                # show the result if in debug mode
-                self.debug_imshow(lp, 'letters')
-                # return the average height of the characters
-                return round(sum(heights) / len(heights), self.accuracy)
-            else:
-                print("no characters in licence plate found")
-                return found
+            if self.max_ar >= ar >= self.min_ar:
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 1)
+                # save h in heights
+                heights.append(h)
+        cv2.imshow("fdsf", img)
+        cv2.waitKey(0)
+        # return the average height of the characters and exit, otherwise try again
+        return round(sum(heights) / len(heights), self.accuracy)
+
+    def distance_to_camera(self, per_height):
+        # workout and return distance from object to camera (per_height = perceived height)
+        return (self.known_height * self.focal_length) / per_height
+
+    def calculate_focal_length(self, lp, known_distance):
+        avg_height = self.get_average_character_height(lp)
+        # focal length = (measured height in pixels * known distance in mm) / known height in mm
+        focal_length = (avg_height * known_distance) / self.known_height
+        self.focal_length = focal_length
+        print(focal_length)
+        return focal_length
 
 
 if __name__ == "__main__":
-    vdc = VehicleDistanceCalculator(debug=True)
-    test_image = cv2.imread('inputs/castle_medow.jpeg')
-    test_image = test_image[668:1911, 653:2223]
-    print(vdc.get_character_height(test_image))
+    vdc = VehicleDistanceCalculator()
+    # ----------- set the focal length -----------
+    # read in the image
+    img = cv2.imread("inputs/straight_2m.png")
+    # find the licence plate
+    lp = vdc.find_licence_plate(img)
+    # calculate and set the focal length
+    vdc.calculate_focal_length(lp, 2000)
+
+    # -------- now test it works on another image ----------
+    img = cv2.imread("inputs/straight_4m.png")
+    lp = vdc.find_licence_plate(img)
+    # get the perceived height of the lp characters
+    height = vdc.get_average_character_height(lp)
+    # calculate the distance
+    distance = vdc.distance_to_camera(height)
+    print(distance)
